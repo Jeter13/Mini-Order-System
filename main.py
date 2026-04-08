db3f62dd3ec2ab5fea3e1d1158a9df4288135d24
Jeter13 <jeterboy2005@gmail.com>
2026-04-08 10:16:51 -0500

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from domain.product import Product
-from domain.order_item import OrderItem
-from domain.order import Order
 from repository.product_repo import ProductRepository
 from repository.order_repo import OrderRepository
+from services.order_service import OrderService
 
 
 def main():
@@ -10,25 +9,21 @@ def main():
     product_repo = ProductRepository()
     order_repo = OrderRepository()
 
+    # Initialize service
+    order_service = OrderService(product_repo, order_repo)
+
     # Add products
     product_repo.add(Product(1, "Mug", 12.50))
     product_repo.add(Product(2, "Scarf", 25.00))
 
-    # Retrieve products
-    mug = product_repo.get(1)
-    scarf = product_repo.get(2)
+    # Create order via service
+    order = order_service.create_order([
+        (1,2),
+        (2,1)
+    ])
 
-    # Create order
-    order = Order(order_id=1)
-    order.add_item(OrderItem(mug, 2))
-    order.add_item(OrderItem(scarf, 1))
-
-    # Save order
-    order_repo.save(order)
-
-    # Retrieve and display order
-    saved_order = order_repo.get(1)
-    saved_order.display()
+    # Display order
+    order.display()
 
 if __name__ == "__main__":
     main()
